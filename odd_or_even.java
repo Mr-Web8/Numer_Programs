440311961c632a6a86d70526699f5917934d3a59
WebB <93731780+Mr-Web8@users.noreply.github.com>
2021-11-06 18:51:03 -0700

    Create odd_or_even.java

---
+++ odd_or_even.java
@@ -0,0 +1,13 @@
+import java.util.*;//using Scanner class from util package
+public class Odd_Even {//change The class name according to the file name
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);//creating an object of Scanner class
+        System.out.println("Enter the number to be checked Number:-");
+        int i=sc.nextInt();//user input
+            if (i%2==0)//check condition
+                System.out.println("Yes, an even number");
+
+            else
+                System.out.println("Yes,a odd number");
+   }
+}
